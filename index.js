21f54994ab8e9e6e83db4ba09f984d4ba1e8d5e7
Neuy <neuwirth.andrew9@gmail.com>
2022-10-17 16:56:03 -0400

    fixed table of contents

--- index.js
+++ index.js
@@ -6,44 +6,44 @@ const generateReadme = ({ Title, Description, Install, Application, Contribution
 
     `#${Title}   
 
-    ${licenseBadge}
+    ${License}
      
 ##Table of contents
 
-###*[Description](#Description)
+*[Description](#Description)
 
-###*[Installation](#Installation)
+*[Installation](#Installation)
 
-###*[How to use](#How-to-use)
+*[How to use](#How-to-use)
 
-###*[How to contribute](#How-to-contribute)
+*[How to contribute](#How-to-contribute)
 
-###*[Tests](#Tests)
+*[Tests](#Tests)
 
-###*[Questions](#Questions)
+*[Questions](#Questions)
 
 -----
-###Description 
+##Description 
 
     ${Description}
 
-###Installation
+##Installation
 
     ${Install}
 -----
-###How to use
+##How to use
 
     ${Application}
 -----
-###How to contribute
+##How to contribute
 
     ${Contribution}
 -----
-###Tests
+##Tests
 
     ${Test}
 -----
-###Questions
+##Questions
 
     [Github](${Github}) Email: <${Email}>`;
 
@@ -101,22 +101,22 @@ inquirer
         const readmePageContent = generateReadme(response);
         console.log(response);
         console.log(readmePageContent);
-        
+        let License = licenseBadge
         switch(response.License) {
             case 'BSD':
-             licenseBadge += '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+             licenseBadge = '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
              break;
              case 'MPL':
-                    licenseBadge += '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+                    licenseBadge = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
                     break;
                     case 'AGPL':
-                        licenseBadge += '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)';
+                        licenseBadge = '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)';
                         break;
                         case 'Public Domain':
-                            licenseBadge += '[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)';
+                            licenseBadge = '[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)';
                             break;
                             case 'Unlicensed':
-                                licenseBadge += 'Unlicensed';
+                                licenseBadge = 'Unlicensed';
         };
 
         fs.writeFile('README.md', readmePageContent, (err) =>
